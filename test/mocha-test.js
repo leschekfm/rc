7a4e27397fd6d932d5aacd16548cdd20e542384a
leschekfm <leschekfm@web.de>
2016-03-12 17:39:20 +0100

    add tests for env vars

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -5,9 +5,30 @@ var assert = require('assert');
 errorWorthyTypes = ['asdf', 1, [], function() { }]
 
 describe('rc', function() {
+  describe('environment variables', function() {
+    before(function() {
+      process.env['anything'] = 'not-avaliable'
+      process.env[n + '_simpleOption'] = 42
+      process.env[n + '_notSoSimpleOption__nested'] = 'crazy'
+      config = rc(n);
+    });
+    after(function() {
+      delete process.env['anything']
+      delete process.env[n + '_simpleOption']
+      delete process.env[n + '_notSoSimpleOption__nested']
+    });
+    it('should add vars prepended with module name', function() {
+      assert.equal(config.simpleOption, 42)
+    });
+    it('should not add vars with other names', function() {
+      assert.notEqual(config.anything, 'not-avaliable')
+    });
+    it('should allow nested variables', function() {
+      assert.equal(config.notSoSimpleOption.nested, 'crazy')
+    })
+  });
   describe('second param (defaults)', function() {
-    it('should thow error for non objects', function() {
-
+    it('should throw error for non objects', function() {
       for (var i = 0; i < errorWorthyTypes.length; i++) {
         assert.throws(
           function() {
